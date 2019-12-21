66f8bc67288e5c116ddb350e8bfead557cc747e2
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2019-12-21 17:29:38 +0200

    FL12_HW6 completed

---
+++ FL12_HW6/homework/js/task1.js
@@ -0,0 +1,25 @@
+//asking user to input values for Quadratic equation
+const a = parseFloat(prompt('please input value of A for Quadratic equation'));
+const b = parseFloat(prompt('please input value of B for Quadratic equation'));
+const c = parseFloat(prompt('please input value of C for Quadratic equation'));
+
+//log error if any of entered values is not valid for Quadratic equation
+let isInputValid;
+if (isNaN(a) || isNaN(b) || isNaN(c) || a===0) {
+  console.log('Invalid input data');
+  isInputValid = false;
+}else{
+  isInputValid = true;
+}
+
+//resolve Quadratic equation if users input is valid and log result
+if (isInputValid) {
+  let d = b*b-4*a*c;
+  if (d>0){
+    console.log(`x1 = ${Math.round((-b+Math.sqrt(d))/2*a)} and x2 = ${Math.round((-b-Math.sqrt(d))/2*a)}`);
+  }else if (d===0){
+    console.log(`x = ${Math.round(-1*b/2*a)}`);
+  } else {
+    console.log('no solution');
+  }
+}
---
+++ FL12_HW6/homework/js/task2.js
@@ -0,0 +1,29 @@
+//asking user to input integer values for triangle sides length
+const a = parseInt(prompt('please input integer value of triangle side A length'));
+const b = parseInt(prompt('please input integer value of triangle side B length'));
+const c = parseInt(prompt('please input integer value of triangle side C length'));
+
+//log error if any of entered values is not valid
+let isInputValid;
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  console.log('input values should be ONLY numbers');
+  isInputValid = false;
+}else if (a<=0 || b<=0 || c<=0) {
+  console.log('A triangle must have 3 sides with a positive definite length');
+  isInputValid = false;
+}else{
+  isInputValid = true;
+}
+
+//log error if triangle cannot be made up, else log triangle type
+if (isInputValid) {
+  if ( a>b+c || b>a+c || c>a+b) {
+    console.log("Triangle doesn't exist");
+  }else if (a===b && a===c){
+    console.log('Equilateral triangle');
+  }else if (a===b || a===c || b===c) {
+    console.log('Isosceles triangle');
+  }else{
+    console.log('Scalene triangle');
+  }
+}
