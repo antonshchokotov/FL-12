a03369db7a801873983a635aac9c8f6a03e0197d
Anton Shchokotov <58229945+antonshchokotov@users.noreply.github.com>
2020-04-04 16:42:27 +0300

    S2-HW13 completed

---
+++ Homework_13/src/js/checkWins.js
@@ -0,0 +1,22 @@
+const cells = document.querySelectorAll('.cell');
+
+const combinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+function checkWin(currentClass) {
+  return combinations.some(combination => {
+    return combination.every(index => {
+      return cells[index].classList.contains(currentClass)
+    })
+  })
+}
+
+export {cells, combinations, checkWin};
